740114d1845284f71b0ff6a55094d84ec9958344
David Bretler <57757495+DavidBretler@users.noreply.github.com>
2021-09-17 11:43:33 +0300

    finish

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "Image_processing.h"
+#include <string>
+int main(int argc, char** argv)
+{
+    std::string videoName = argv[1];
+    std::string textFileName = argv[2];
+    std::string imageFolderName = argv[3];
+    std::string bedNumber = argv[4];
+
+    childrenRec(videoName, textFileName, imageFolderName, std::stoi(bedNumber));
+    return 0;
+}
